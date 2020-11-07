f23c8799b820d3c17c99750a10855e6165746d3d
noman13bd <noman13bd@gmail.com>
2020-11-07 19:26:17 +0600

    code updated

--- server.py
+++ server.py
@@ -1,9 +1,11 @@
 from src.core.database import app
 from src.resource.home import home_blueprint
 from src.resource.user import user_blueprint
+from src.resource.todo import todo_blueprint
 
 app.register_blueprint(home_blueprint)
 app.register_blueprint(user_blueprint)
+app.register_blueprint(todo_blueprint)
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
--- src/utils/token.py
+++ src/utils/token.py
@@ -7,10 +7,10 @@ from typing import Dict, Any
 class TokenGenerator:
     @staticmethod
     def generate_jwt(payload: Dict[str, Any], minutes: int = 60):
-        token = jwt.encode({
-            'exp': datetime.datetime.utcnow() + datetime.timedelta(minutes = minutes),
-            'name': payload['name'],
-            'email': payload['email']
-        }, app.config['SECRET_KEY'])
+        payload['exp'] = datetime.datetime.utcnow() + datetime.timedelta(minutes = minutes)
+        token = jwt.encode(
+            payload
+            , app.config['SECRET_KEY']
+        )
         
         return token.decode('utf-8')
